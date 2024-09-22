d469dfadb1f051d8939a96686dd9826b5d01ef26
radom1789 <tracyy@andrew.cmu.edu>
2024-09-22 12:11:57 -0400

    Wrote search function in search file for Topics

---
+++ src/topics/search.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const db = require('./database');
+const posts = require('../posts');
+
+module.exports = function (Topics) {
+	Topics.search = async function (query, options) {
+		if (!query) {
+			return [];
+		}
+		query = String(query).toLowerCase();
+
+        // Fetch all topic IDs (or a subset if using pagination)
+        const allTids = await db.getSortedSetRevRange('topics:recent', 0, 1000);
+
+        // Fetch topic data based on the IDs
+        let topics = await Topics.getTopicsByTids(allTids, { uid: options.uid });
+	
+        // Filter topics by search query
+        topics = topics.filter(topic => {
+            return topic && (
+                (topic.title && topic.title.toLowerCase().includes(query)) ||
+                (topic.description && topic.description.toLowerCase().includes(query))
+            );
+        });
+
+        // Do sorting here
+        if (options.sort === 'newest') {
+            topics = _.orderBy(topics, ['timestamp'], ['desc']);
+        } else if (options.sort === 'oldest') {
+            topics = _.orderBy(topics, ['timestamp'], ['asc']);
+        }
+
+        return topics;
+    };
+};
