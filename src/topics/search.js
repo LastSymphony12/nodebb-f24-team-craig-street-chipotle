96529b84553ff51b926d50f384376915c4393eeb
Alex Lewis <adlewis@andrew.cmu.edu>
2024-09-24 13:55:04 -0400

    fixed code issues in search.js

--- src/topics/search.js
+++ src/topics/search.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const db = require('./database');
-const posts = require('../posts');
+const _ = require('lodash');
+const db = require('../database');
 
 module.exports = function (Topics) {
 	Topics.search = async function (query, options) {
@@ -10,27 +10,25 @@ module.exports = function (Topics) {
 		}
 		query = String(query).toLowerCase();
 
-        // Fetch all topic IDs (or a subset if using pagination)
-        const allTids = await db.getSortedSetRevRange('topics:recent', 0, 1000);
+		// Fetch all topic IDs (or a subset if using pagination)
+		const allTids = await db.getSortedSetRevRange('topics:recent', 0, 1000);
 
-        // Fetch topic data based on the IDs
-        let topics = await Topics.getTopicsByTids(allTids, { uid: options.uid });
-	
-        // Filter topics by search query
-        topics = topics.filter(topic => {
-            return topic && (
-                (topic.title && topic.title.toLowerCase().includes(query)) ||
+		// Fetch topic data based on the IDs
+		let topics = await Topics.getTopicsByTids(allTids, { uid: options.uid });
+
+		// Filter topics by search query
+		topics = topics.filter(topic => topic && (
+			(topic.title && topic.title.toLowerCase().includes(query)) ||
                 (topic.description && topic.description.toLowerCase().includes(query))
-            );
-        });
+		));
 
-        // Do sorting here
-        if (options.sort === 'newest') {
-            topics = _.orderBy(topics, ['timestamp'], ['desc']);
-        } else if (options.sort === 'oldest') {
-            topics = _.orderBy(topics, ['timestamp'], ['asc']);
-        }
+		// Do sorting here
+		if (options.sort === 'newest') {
+			topics = _.orderBy(topics, ['timestamp'], ['desc']);
+		} else if (options.sort === 'oldest') {
+			topics = _.orderBy(topics, ['timestamp'], ['asc']);
+		}
 
-        return topics;
-    };
+		return topics;
+	};
 };
