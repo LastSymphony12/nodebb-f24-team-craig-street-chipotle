efed1e8bb51601359900078c4baa6dc394890d4b
Caroline Michalow <cmichalo@andrew.cmu.edu>
2024-10-10 22:07:11 -0400

    moved anonymous variable out of the post data to avoid schema error. Allowed anonymous to be optional in the schema

--- src/topics/create.js
+++ src/topics/create.js
@@ -35,7 +35,6 @@ module.exports = function (Topics) {
 			lastposttime: 0,
 			postcount: 0,
 			viewcount: 0,
-			anonymous: anonymous, // line for creating a tag for anonymous function
 		};
 
 		// this line is to see console output
